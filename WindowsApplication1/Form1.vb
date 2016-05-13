183867e3ead36993a8fc7eeecdd76f4ad29b1281
Brandon Behnen <the1337moderator@gmail.com>
2016-05-13 16:08:03 -0500

    added comments

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,13 +1,15 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Populate the DataSet
+
+        ' Create rows in the foo table
         Dim fooRow1 As DS.fooRow = Me.Ds1.foo.AddfooRow("hoopla")
         Dim fooRow2 As DS.fooRow = Me.Ds1.foo.AddfooRow("blargh")
 
+        ' Create rows in the bar table
         Me.Ds1.bar.AddbarRow(fooRow1, "cheese")
         Me.Ds1.bar.AddbarRow(fooRow2, "chicken")
         Me.Ds1.bar.AddbarRow(fooRow1, "sprinkles")
         Me.Ds1.bar.AddbarRow(fooRow2, "soup")
-
-
     End Sub
 End Class
